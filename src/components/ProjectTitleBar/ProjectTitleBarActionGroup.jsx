0abc3ce0287d7e868a9d3c3b8a7ca7a27204b6ce
wwang33 <wwang33@ncsu.edu>
2021-07-03 22:17:40 -0700

    allows save and load from computer

--- src/components/ProjectTitleBar/ProjectTitleBarActionGroup.jsx
+++ src/components/ProjectTitleBar/ProjectTitleBarActionGroup.jsx
@@ -1,16 +1,34 @@
-import {Dropdown} from "antd";
+import {Dropdown, Menu} from "antd";
 import {IconButton, Tooltip} from "@material-ui/core";
-import {Home, SaveAlt} from "@material-ui/icons";
+import {Home, SaveAlt, InsertDriveFile} from "@material-ui/icons";
 import React from "react";
+import {CopyOutlined, DeleteOutlined, DownloadOutlined, ImportOutlined} from "@ant-design/icons";
+// import InsertDriveFile from "@material-ui/core/Icon";
+
+const actionMenu =
+    (
+        <Menu theme="dark"
+        >
+            <Menu.Item
+                key="1">
+                Load from computer</Menu.Item>
+            <Menu.Item
+                key="2" >
+                Save to computer
+            </Menu.Item>
+        </Menu>
+    );
+
 
 const ProjectTitleBarActionGroup  = () => {
     return (
         <>
-            <Tooltip title="Save">
-                <IconButton aria-label="display more actions" color="inherit" size="medium">
-                    <SaveAlt/>
-                </IconButton>
-            </Tooltip>
+            <Dropdown overlay={actionMenu}>
+                    <IconButton aria-label="files" size="medium">
+                        <InsertDriveFile style={{color: "white"}}/>
+                    </IconButton>
+            </Dropdown>
+
             <Tooltip title="Back to home">
                 <IconButton aria-label="display more actions" color="inherit" size="medium">
                     <Home/>
