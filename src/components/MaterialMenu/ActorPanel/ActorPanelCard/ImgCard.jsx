8ad6d1726d0df6fdbc3754d8462fee5f4407eac0
wwang33 <wwang33@ncsu.edu>
2021-07-11 13:51:15 -0700

    added option to use and delete

--- src/components/MaterialMenu/ActorPanel/ActorPanelCard/ImgCard.jsx
+++ src/components/MaterialMenu/ActorPanel/ActorPanelCard/ImgCard.jsx
@@ -8,6 +8,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import {useDispatch} from "react-redux";
 import {deleteState} from "../../../../redux/features/projectSlice";
+import {IconButton} from "@material-ui/core";
+import MoreIcon from "@material-ui/core/SvgIcon/SvgIcon";
+import {ArrowForward, DeleteOutlined} from "@material-ui/icons";
+import Fab from "@material-ui/core/Fab";
+import Tooltip from "@material-ui/core/Tooltip";
 
 const useStyles = makeStyles({
     root: {
@@ -18,20 +23,50 @@ const useStyles = makeStyles({
         position: 'relative',
         paddingBottom: props => props.heightToWidthRatio,
         paddingTop: '0',
-        height: 0
+        height: 0,
     },
     elementToStretch: {
         position: 'absolute',
         top: 0,
         left: 0,
         width: '100%',
-        height: '100%'
+        height: '100%',
     },
     imgStyle: {
         objectFit: 'contain',
         width: '100%',
-        height: '100%'
-    }
+        height: '100%',
+
+
+
+    },
+    divOverlap: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: 1,
+        width: "100%",
+        height: "100%",
+        background: "rgba(0,0,0,0.6)",
+        opacity: 1,
+        cursor: "auto",
+    },
+    buttonOverlapUse: {
+        position: "relative",
+        zIndex: 2,
+        left: "15%",
+        top: "30%",
+        width: "35px",
+        height: "35px",
+    },
+    buttonOverlapDelete: {
+        position: "relative",
+        zIndex: 2,
+        left: "20%",
+        top: "30%",
+        width: "35px",
+        height: "35px",
+    },
 });
 
 
@@ -39,9 +74,9 @@ const ImgCard = (props) =>  {
     const {actorId, stateId, imgSrc, contentNode } = props;
     const classes = useStyles(props);
     const dispatch  = useDispatch();
+    const [onHover, setOnHover] = React.useState();
 
-
-    const handleClick = () => {
+    const handleDelete = () => {
         console.log("stateId: ", stateId)
         dispatch(deleteState(
             {actorId,
@@ -50,32 +85,57 @@ const ImgCard = (props) =>  {
     }
 
 
-    const menu = () => {
-        return (<Menu>
-            <Menu.Item
-                key="1"
-                onClick={handleClick}
-            >delete state</Menu.Item>
-        </Menu>)
-    };
+    // const menu = () => {
+    //     return (<Menu>
+    //         <Menu.Item
+    //             key="1"
+    //             onClick={handleClick}
+    //         >delete state</Menu.Item>
+    //     </Menu>)
+    // };
 
 
     return (
         <Card
             variant="outlined"
             className={classes.root}>
-            <Dropdown overlay={menu(stateId)} trigger={['contextMenu']}>
-            <CardMedia className={classes.media}>
-                    <div className={classes.elementToStretch}>
+            {/*<Dropdown overlay={menu(stateId)} trigger={['contextMenu']}>*/}
+            <CardMedia className={classes.media}
+                       onMouseEnter={() => {setOnHover(true)}}
+                       onMouseLeave={() =>{ setOnHover(false)}}
+            >
+                    <div className={classes.elementToStretch}
+                    >
                         <img
                             draggable
                             className={classes.imgStyle}
                             src={imgSrc}
                             alt="img"
                         />
+                        <div className={classes.divOverlap} style={{display: onHover? "block":"none" }}>
+                            <Tooltip title="Use">
+                            <IconButton aria-label="add"
+                                        className={classes.buttonOverlapUse}
+                                        color="inherit"
+                                        variant="contained"
+                                        size="small">
+                                <ArrowForward style={{color: "white"}} />
+                            </IconButton>
+                            </Tooltip>
+                            <Tooltip title="Delete">
+                                <IconButton aria-label="add"
+                                     className={classes.buttonOverlapDelete}
+                                     color="inherit"
+                                     variant="contained"
+                                     size="small"
+                                     onClick={handleDelete}>
+                                    <DeleteOutlined style={{color: "white"}} />
+                                </IconButton>
+                            </Tooltip>
+                        </div>
                     </div>
                 </CardMedia>
-            </Dropdown>
+            {/*</Dropdown>*/}
             {contentNode}
         </Card>
     );
