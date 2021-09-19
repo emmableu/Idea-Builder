439460f93e88127cf769e7860e152d0e0276aec1
wwang33 <wwang33@ncsu.edu>
2021-09-18 22:00:51 -0400

    allows showing costume swapper

--- src/components/StoryboardMenu/CostumeSwapperStepper.jsx
+++ src/components/StoryboardMenu/CostumeSwapperStepper.jsx
@@ -83,7 +83,7 @@ const useStyles = makeStyles((theme) => ({
         alignItems: "center",
     },
     stepsContent: {
-        width: "60%",
+        width: "100%",
         height: globalConfig.costumeSwapperHeight,
         // marginTop: 16,
         // padding: "8px 8px",
@@ -149,25 +149,28 @@ const getSelectedRecommend = createSelector(
     state => state.recommend.value.currentCostumes,
     (actorList,backdropList, selected, originalCostumes, currentCostumes,
      ) => {
-
-
         const userCostumes = [];
+        for (const backdrop of backdropList) {
+            userCostumes.push({
+                type: "backdrop",
+                ...backdrop
+            })
+        }
         for (const actor of actorList) {
-            if (actor.deleted !== true ) {
-                for (const state of actor.stateList) {
-                    userCostumes.push(
-                        {
-                            actorId: actor._id,
-                            ...state,
-                            actorName: actor.name
-                        }
-                    )
-                }
+            for (const state of actor.stateList) {
+                userCostumes.push(
+                    {
+                        actorId: actor._id,
+                        actorName: actor.name,
+                        ...state,
+                        type: "state",
+                    }
+                )
             }
         }
+
         return {
             originalCostumes, userCostumes,
-        userBackdrops: backdropList,
             currentCostumes,
         }
     }
@@ -183,6 +186,7 @@ const CostumeSwapperStepper = (props) => {
     const classes = useStyles();
     const {
         setCurrent,
+        userCostumes,
         originalCostumes,
         currentCostumes,
     } = props;
@@ -190,24 +194,47 @@ const CostumeSwapperStepper = (props) => {
     const [selected, setSelected] = React.useState(null);
     const dispatch = useDispatch();
 
-    const reset = (e) => {
-        dispatch(resetModifiedRecommend());
-    };
-
-    const handleUse = (e, actorId, _id) => {
-        // console.log("handleUse", selected, originalCostumes.length, originalBackdrops.length)
-        dispatch(modifyRecommend(
-            {
-                newActorId: actorId,
-                newStateId: _id,
-                selected,
-            }))
-
-    };
+    // const reset = (e) => {
+    //     dispatch(resetModifiedRecommend());
+    // };
+    //
+    // const handleUse = (e, actorId, _id) => {
+    //     // console.log("handleUse", selected, originalCostumes.length, originalBackdrops.length)
+    //     dispatch(modifyRecommend(
+    //         {
+    //             newActorId: actorId,
+    //             newStateId: _id,
+    //             selected,
+    //         }))
+    //
+    // };
 
     return (
         <>
             <div className={classes.cardList}>
+                <Card
+                    // style={{ width: 300 }}
+                    size="small"
+                    hoverable
+                    bordered
+                    style={{
+                        padding: "1px 1px",
+                        height: "120px",
+                        width: `${120*0.44}px`,
+                        margin: "5px 5px"
+                    }}
+                    bodyStyle={{padding: "0 0",
+                    }}
+                >
+                    <div style={{overflow: "hidden",
+                    height: `${120*0.44}px`, display: "block",
+                    whiteSpace: "nowrap",
+                    textOverflow: "clip"}}>Original</div>
+                    <div style={{overflow: "hidden",
+                        height: `${120*0.44}px`, display: "block",
+                        whiteSpace: "nowrap",
+                        textOverflow: "clip"}}>New</div>
+                </Card>
                 {
                     currentCostumes.map((currentCostume, idx) => (
                         <SwapperCard
@@ -222,9 +249,77 @@ const CostumeSwapperStepper = (props) => {
                     ))
                 }
             </div>
+            {
+                selected !== null &&
+                <Swapper
+                    currentCostume={originalCostumes[selected]}
+                    userCostumes={userCostumes}
+                    selected={selected}
+                />
+            }
         </>
     )
 }
+
+
+const Swapper = React.memo((props) => {
+    const classes = useStyles();
+    const {currentCostume, selected, userCostumes} = props;
+    const {type} = currentCostume;
+    // newActorId, newStateId, selected
+    const dispatch = useDispatch();
+    const handleUse = (e, actorId, _id) => {
+        dispatch(modifyRecommend(
+            {
+                newActorId: actorId,
+                newStateId: _id,
+                selected,
+            }
+        ));
+    };
+
+
+
+    return (
+        <div className={classes.stepsContent}>
+            <div  className={classes.userCostumes}>
+                <div>
+                    {"My backdrops"}
+                </div>
+                <Grid container spacing={1} justifyContent="center">
+
+                    {userCostumes.map(imgData => (
+                        <Grid item xs={4}
+                              key={imgData._id}
+                        >
+                            <ImgTile
+                                type="swap-costume-backdrop"
+                                _id={imgData._id}
+                                name={imgData.name}
+                                imgSrc={axios.defaults.baseURL + imgData._id}
+                                heightToWidthRatio="70%"
+                                handleDelete={null}
+                                handleUse={handleUse}
+                                contentNode={imgData.name}
+                            />
+                        </Grid>
+                    ))}
+                </Grid>
+            </div>
+            <div
+                className={classes.assetGallery}
+            >
+                {`Search for a new ${type === "backdrop"? "backdrop": "actor"}`}
+                <AssetGallery
+                    xs={4}
+                    height={globalConfig.costumeSwapperHeight-50}
+                    type={type}
+                    itemSize={120}
+                />
+            </div>
+            </div>
+    )
+})
 
 
 const SwapperCard = React.memo(( props
@@ -239,8 +334,8 @@ const SwapperCard = React.memo(( props
             style={{
                 padding: "1px 1px",
                 border: order === selected? "2px solid orange":"1px solid #e0e0e0",
-                height: "100px",
-                width: '50px',
+                height: "120px",
+                width: `${120*0.44}px`,
                 margin: "5px 5px"
             }}
             cover={
@@ -249,22 +344,29 @@ const SwapperCard = React.memo(( props
                     style={{display: "flex", flexDirection: "column"}}
                 >
                     <div
-                        style = {{height: "50%", width: "100%", border:"1px solid #e0e0e0" }}
+                        style = {{height: "44%", width: "100%", border:"1px solid #e0e0e0" }}
                     >
                     <img src={axios.defaults.baseURL + originalCostume._id} alt="original"
                          style = {{height: "100%", width: "100%"}}
                     />
                     </div>
-                    <img src={axios.defaults.baseURL + currentCostume._id} alt="current"
-                         style = {{height: "50%", width: "100%"}}
-                    />
+                    <div
+                        style = {{height: "44%", width: "100%", border:"1px solid #e0e0e0" }}
+                    >
+                        <img src={axios.defaults.baseURL + currentCostume._id} alt="current"
+                             style = {{height: "100%", width: "100%"}}
+                        />
+                    </div>
                 </div>
             }
             bodyStyle={{padding: "0 0",
             }}
         >
             <Meta
-                description={`actor: ${originalCostume.actorName} \n state: ${originalCostume.name}`}
+                description={<div style={{overflow: "hidden",
+                    height: "100%", display: "block",
+                    whiteSpace: "nowrap",
+                    textOverflow: "clip"}}>{originalCostume.actorName? originalCostume.actorName : "stage"}</div>}
             />
         </Card>
     )
